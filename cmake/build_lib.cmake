83aed083c4257bc5e5dbe23bfb2988c87d1eefa9
Jordan Hayes <jhayes5@ncsu.edu>
2023-12-22 05:21:55 +0000

    Moved silencing warnings to this repo

--- cmake/build_lib.cmake
+++ cmake/build_lib.cmake
@@ -11,7 +11,14 @@ function(build_physics_backend)
             ${CMAKE_SOURCE_DIR}/include
         PRIVATE
             ${CMAKE_SOURCE_DIR}/src
-    ) 
+    )
+
+    # Silence warnings
+    if (CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        target_compile_options(units INTERFACE -w)
+    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+        target_compile_options(units INTERFACE /w)
+    endif()
 
     target_link_libraries(physics_backend
         PRIVATE
