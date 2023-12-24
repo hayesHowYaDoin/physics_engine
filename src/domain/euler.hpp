4f8f26d40f135684e5cf4197de849e976b456c3e
Jordan Hayes <jordanhayes98@gmail.com>
2023-12-24 00:33:08 +0000

    Filled in remaining euler motion equations

--- src/domain/euler.hpp
+++ src/domain/euler.hpp
@@ -7,16 +7,34 @@
 namespace domain::euler
 {
 
-    template <core::IsLengthUnit LengthType, core::IsVelocityUnit VelocityType, core::IsTimeUnit TimeType>
-    PositionVector2D<LengthType> nextPosition(
-        PositionVector2D<LengthType> const& position,
-        VelocityVector2D<VelocityType> const& velocity,
-        TimeType const& dt)
-    {
-        return Vector2D::fromComponents(
-            position.getX<LengthType>() + velocity.getX<VelocityType>() * dt,
-            position.getY<LengthType>() + velocity.getY<VelocityType>() * dt);
-    }
+template <core::IsLengthUnit LengthType, core::IsVelocityUnit VelocityType, core::IsTimeUnit TimeType>
+[[nodiscard]] constexpr
+PositionVector2D<LengthType> nextPosition(
+    PositionVector2D<LengthType> const& position,
+    VelocityVector2D<VelocityType> const& velocity,
+    TimeType const& dt)
+{
+    return position + velocity * dt;
+}
+
+template <core::IsVelocityUnit VelocityType, core::IsAccelerationUnit AccelerationType, core::IsTimeUnit TimeType>
+[[noexcept]] constexpr
+VelocityVector2D<VelocityType> nextVelocity(
+    VelocityVector2D<VelocityType> const& velocity,
+    AccelerationVector2D<AccelerationType> const& acceleration,
+    TimeType const& dt)
+{
+    return velocity + acceleration * dt;
+}
+
+template <core::IsAccelerationUnit AccelerationType, core::IsForceUnit ForceType, core::IsMassUnit MassType>
+[[nodiscard]] constexpr
+AccelerationVector2D<AccelerationType> acceleration(
+    ForceVector2D<ForceType> const& force,
+    MassType const& mass)
+{
+    return force / mass;
+}
 
 } // namespace domain::euler
 
