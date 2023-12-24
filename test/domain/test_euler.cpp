63929b3fa66284b6f2eac1b4842c09cab647873c
Jordan Hayes <jordanhayes98@gmail.com>
2023-12-24 02:24:09 +0000

    Created test for euler acceleration equation

--- test/domain/test_euler.cpp
+++ test/domain/test_euler.cpp
@@ -9,8 +9,8 @@ static constexpr auto REQUIRED_PRECISION {0.000'001f};
 
 TEST(EulerTest, NextPosition)
 {
-    using namespace units::literals;
     using namespace domain;
+    using namespace units::literals;
 
     constexpr auto position {Vector2D::fromComponents(10.0_m, 10.0_m)};
     constexpr auto velocity {Vector2D::fromComponents(10.0_mps, 10.0_mps)};
@@ -31,19 +31,36 @@ TEST(EulerTest, NextPosition)
 
 TEST(EulerTest, NextVelocity)
 {
+    using namespace domain;
     using namespace units::literals;
     using namespace units::velocity;
-    using namespace domain;
 
     constexpr auto velocity {Vector2D::fromComponents(10.0_mps, 10.0_mps)};
     constexpr auto acceleration {Vector2D::fromComponents(10.0_mps_sq, 10.0_mps_sq)};
     constexpr auto dt {1.0_s};
 
-    constexpr auto nextVelocity {euler::nextVelocity(velocity, acceleration, dt)};
+    constexpr auto next {euler::nextVelocity(velocity, acceleration, dt)};
 
-    constexpr auto x {nextVelocity.getX<meters_per_second<float>>()};
+    constexpr auto x {next.getX<meters_per_second<float>>()};
     EXPECT_NEAR(x.to<float>(), 20.0f, REQUIRED_PRECISION);
 
-    constexpr auto y {nextVelocity.getY<meters_per_second<float>>()};
+    constexpr auto y {next.getY<meters_per_second<float>>()};
     EXPECT_NEAR(y.to<float>(), 20.0f, REQUIRED_PRECISION);
+}
+
+TEST(EulerTest, Acceleration)
+{
+    using namespace domain;
+    using namespace units::literals;
+    using mpssUnits = units::acceleration::meters_per_second_squared<double>;
+
+    constexpr auto force {Vector2D::fromComponents(10.0_N, 10.0_N)};
+    constexpr auto mass {10.0_kg};
+
+    constexpr auto acceleration {euler::acceleration<mpssUnits>(force, mass)};
+    constexpr auto x {acceleration.getX<mpssUnits>()};
+    EXPECT_NEAR(x.to<float>(), 1.0f, REQUIRED_PRECISION);
+
+    constexpr auto y {acceleration.getY<mpssUnits>()};
+    EXPECT_NEAR(y.to<float>(), 1.0f, REQUIRED_PRECISION);
 }
