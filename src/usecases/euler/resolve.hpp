857e87e9c0ee209cc794e8d8278ec2339e488683
Jordan Hayes <jordanhayes98@gmail.com>
2023-12-26 18:54:58 +0000

    Made step function which takes a range and motion resolve transformation

--- src/usecases/euler/resolve.hpp
+++ src/usecases/euler/resolve.hpp
@@ -22,13 +22,27 @@ EulerParticle resolveMotion(
 {
     using namespace units::literals;
 
-    auto forcesSum {std::accumulate(particle.forces.begin(), particle.forces.end(), domain::ForceVector2D {0.0_N, 0.0_N})};
+    auto forcesSum {std::accumulate(particle.forces.begin(), particle.forces.end(), domain::ForceVector2D(0.0_N, 0.0_N))};
 
     auto acceleration {domain::motion::acceleration<units::acceleration::meters_per_second_squared<double>>(forcesSum, particle.mass)};
     auto velocity {domain::motion::nextVelocity(particle.velocity, acceleration, timeStep)};
     auto position {domain::motion::nextPosition(particle.position, velocity, timeStep)};
 
-    return EulerParticle {position, velocity, particle.mass};
+    return EulerParticle {
+        .position = position,
+        .velocity = velocity,
+        .mass = particle.mass,
+        .forces = particle.forces};
+}
+
+template <core::IsTimeUnit TimeUnit>
+[[nodiscard]] constexpr
+auto step(
+    std::ranges::range auto const& particles,
+    TimeUnit const& timeStep,
+    std::function<EulerParticle(EulerParticle const&)> const& resolveMotion)
+{
+    return particles | std::views::transform(resolveMotion);
 }
 
 } // namespace usecases::euler
--- test/usecases/euler/test_resolve.cpp
+++ test/usecases/euler/test_resolve.cpp
@@ -4,7 +4,7 @@
 #include <units.h>
 #include <gtest/gtest.h>
 
-static constexpr auto const REQUIRED_PRECISION {0.000'001};
+static constexpr auto REQUIRED_PRECISION {0.000'001f};
 
 TEST(EulerResolve, resolveMotion)
 {
@@ -22,6 +22,39 @@ TEST(EulerResolve, resolveMotion)
     auto const timeStep {units::time::seconds<double> {1}};
 
     auto const result {usecases::euler::resolveMotion(particle, timeStep)};
+
+    EXPECT_TRUE(domain::Vector2D::compare(
+        result.position,
+        domain::PositionVector2D(0.0_m, 0.19_m),
+        REQUIRED_PRECISION));
+    EXPECT_TRUE(domain::Vector2D::compare(
+        result.velocity,
+        domain::VelocityVector2D(0.0_mps, -9.81_mps),
+        REQUIRED_PRECISION));
+    EXPECT_NEAR(result.mass.to<float>(), 1.0f, REQUIRED_PRECISION);
+}
+
+TEST(EulerResolve, step)
+{
+    using namespace units::literals;
+
+    auto const particles {std::vector<usecases::EulerParticle> {
+        usecases::EulerParticle {
+            domain::PositionVector2D(0.0_m, 10.0_m),
+            domain::VelocityVector2D(0.0_mps, 0.0_mps),
+            1.0_kg,
+            {domain::ForceVector2D(0.0_N, domain::constants::G * 1.0_kg)}
+        }
+    }};
+    auto const timeStep {units::time::seconds<double> {1}};
+    auto const resolveMotion {[&timeStep](usecases::EulerParticle const& particle) {
+        return usecases::euler::resolveMotion(particle, timeStep);
+    }};
+
+    auto const results {usecases::euler::step(particles, timeStep, resolveMotion)};
+    EXPECT_TRUE(results.size() == 1);
+
+    auto const result {results[0]};
 
     EXPECT_TRUE(domain::Vector2D::compare(
         result.position,
