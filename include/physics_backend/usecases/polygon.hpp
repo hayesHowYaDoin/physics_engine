da37b30672715c54973451111dfc7a557fee4891
Jordan Hayes <jordanhayes98@gmail.com>
2024-02-22 02:13:51 +0000

    Added boundary calculation and getters to Polygon2D

--- include/physics_backend/usecases/polygon.hpp
+++ include/physics_backend/usecases/polygon.hpp
@@ -11,6 +11,12 @@ namespace physics::usecases
 template <physics::units::IsLengthUnit Length>
 class Polygon2D
 {
+    struct Boundaries
+    {
+        Length lower;
+        Length upper;
+    };
+
     Polygon2D(std::vector<physics::domain::Vector2D<Length>> vertices): 
         vertices(std::move(vertices))
     {
@@ -25,6 +31,12 @@ class Polygon2D
         m_edges.push_back({m_vertices.back(), m_vertices.front()});
         for(auto vertex {m_vertices.begin()+1}; vertex != m_vertices.end(); ++vertex)
         {
+            m_xBoundaries.lower = std::min(m_xBoundaries.lower, vertex.getX());
+            m_xBoundaries.upper = std::max(m_xBoundaries.upper, vertex.getX());
+
+            m_yBoundaries.lower = std::min(m_yBoundaries.lower, vertex.getY());
+            m_yBoundaries.upper = std::max(m_yBoundaries.upper, vertex.getY());
+
             m_edges.push_back({*(vertex-1), *vertex});
         }
     }
@@ -39,9 +51,21 @@ class Polygon2D
         return m_edges;
     }
 
+    [[nodiscard]] std::pair getXBoundaries() const
+    {
+        return m_xBoundaries;
+    }
+
+    [[nodiscard]] std::pair getYBoundaries() const
+    {
+        return m_yBoundaries;
+    }
+
 private:
     std::vector<physics::domain::Vector2D<Length>> m_vertices;
     std::vector<physics::domain::Edge2D<Length>> m_edges;
+    std::pair<Length, Length> m_xBoundaries;
+    std::pair<Length, Length> m_yBoundaries;
 };
 
 }
