81802ed5dc269243fe8965e89458edef93af32a3
Jordan Hayes <jordanhayes98@gmail.com>
2024-02-25 19:14:58 +0000

    Cleaned up new constrain file

---
+++ include/physics_backend/usecases/constrain.hpp
@@ -0,0 +1,11 @@
+#ifndef PHYSICS_BACKEND_USECASES_EULER_CONSTRAIN_HPP
+#define PHYSICS_BACKEND_USECASES_EULER_CONSTRAIN_HPP
+
+namespace physics::usecases
+{
+
+// TODO: Implement the Constrain use case
+
+} // namespace physics::usecases
+
+#endif // PHYSICS_BACKEND_USECASES_EULER_CONSTRAIN_HPP
