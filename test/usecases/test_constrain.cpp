07c1e01a91265abd45bc75e5d57c1c87af91cf28
Jordan Hayes <jordanhayes98@gmail.com>
2024-02-28 14:13:45 +0000

    Added unit test for ensuring that particles that are in-bounds will not be modified by the constraint

--- test/usecases/test_constrain.cpp
+++ test/usecases/test_constrain.cpp
@@ -33,6 +33,34 @@ TEST(ConstrainTest, CenterInPeekingOut)
     EXPECT_TRUE(physics::domain::Vector2D::compare(updatedParticle.position, expectedPosition, physics::test::REQUIRED_PRECISION));
 }
 
+TEST(ConstrainTest, InBounds)
+{
+    using namespace physics::units::literals;
+    using Length = physics::units::SI::Length;
+
+    physics::usecases::Polygon2D<Length> constraint {
+        {
+            physics::domain::PositionVector2D(0.0_m, 0.0_m),
+            physics::domain::PositionVector2D(1.0_m, 0.0_m),
+            physics::domain::PositionVector2D(1.0_m, 1.0_m),
+            physics::domain::PositionVector2D(0.0_m, 1.0_m)
+        }
+    };
+
+    physics::euler::Particle<physics::units::SI> particle {
+        .mass {1.0_kg},
+        .radius {0.25_m},
+        .position {physics::domain::PositionVector2D(0.5_m, 0.5_m)},
+        .velocity {physics::domain::VelocityVector2D(0.0_mps, 0.0_mps)},
+        .forces {physics::domain::ForceVector2D(0.0_N, -9.81_N)}
+    };
+
+    auto updatedParticle {physics::usecases::resolveConstraint(particle, constraint)};
+    auto expectedPosition {physics::domain::PositionVector2D(0.5_m, 0.5_m)};
+
+    EXPECT_TRUE(physics::domain::Vector2D::compare(updatedParticle.position, expectedPosition, physics::test::REQUIRED_PRECISION));
+}
+
 TEST(ConstrainTest, Tunneling)
 {
     // TODO: Solve the tunneling problem.
